032e441fe9a924176dbc85cf9597dda8420367b9
Jesus3736 <Jesusrodriguez8441@gmail.com>
2026-09-10 10:01:17 -0600

    update

--- Unidad1/05_Hexadecimal_compacto.py
+++ Unidad1/05_Hexadecimal_compacto.py
@@ -1,11 +1,8 @@
-numero = 8
-if numero == 0:
-    print("0")
-hexadecimal = ""
-letras = "0123456789ABCDEF"
+numero = 8# Pedimos al usuario que ingrese un número
+hexadecimal = ""# Creamos una cadena vacía para guardar el resultado en hexadecimal
+letras = "0123456789ABCDEF"# Aquí están los símbolos que usamos en hexadecimal (0-9 y A-F)
 while numero > 0:
-    residuo = numero % 16
-    hexadecimal = letras[residuo] + hexadecimal
+    hexadecimal = letras[numero % 16] + hexadecimal# Dividimos el número entre 16 repetidamente para obtener cada dígito
     numero = numero // 16
-print(hexadecimal)
+print(hexadecimal)# Mostramos el resultado final
 
